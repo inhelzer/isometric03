7c075ed20b47e32e2daf5536388557af686c89fc
inhelzer <121232928+inhelzer@users.noreply.github.com>
2024-01-27 18:05:38 +0200

    gefen

---
+++ iso03/Assets/Scripts/GeLift.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeLift : MonoBehaviour
+{
+    [SerializeField] Vector3 endPoint;
+    [SerializeField] float speed;
+    Vector3 moveVector;
+    [SerializeField] bool isActive = false;
+    [SerializeField] bool isLifting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveVector = (endPoint - transform.position).normalized * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((isActive)/* && (FindObjectOfType<Movement>().isMoving == false)*/)
+        {
+            isLifting = true;
+            FindObjectOfType<Movement>().isLifting = true;
+        }
+
+        if (isLifting)
+        {
+            if (Vector3.Distance(transform.position, endPoint) >= 0.1f)
+            {
+                transform.position += moveVector * Time.deltaTime;
+                moveVector = (endPoint - transform.position).normalized * speed;
+            }
+            else
+            {
+                transform.position = endPoint;
+                isActive = false;
+                isLifting = false;
+                FindObjectOfType<Movement>().StopLifting(transform.position);
+
+            }
+
+            FindObjectOfType<Movement>().liftingPos = transform.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+}
+
